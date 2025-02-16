f23eed21e14bf725408b460b19a57c152046d548
Alexis Vega <alexismouwid@gmail.com>
2025-02-16 16:05:46 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
+const User = require('./user')
+
+mongoose.connect('mongodb+srv://alexismouwid:yosoyotaku23.@intro-mondodb.19ttj.mongodb.net/?retryWrites=true&w=majority&appName=Intro-Mondodb')
+      .then(() => console.log('Conectado a MongoDB'))
+      .catch(err => {
+        console.error('Error al conectar a MongoDB:', err.message);
+        process.exit(1); // Detiene la app si falla la conexión
+      });
+
+const app = express()
+
+app.use(express.json())
+
+const signToken = _id =>  jwt.sign({ _id }, 'mi-string-secreto')
+
+app.post('/register', async (req, res) => {
+  const { body } = req
+  try {
+        const isUser = await User.findOne( { email: body.email } )
+          if (isUser)
+              {
+                 return res.status(403).send('Usuario ya existe')
+              }
+          const salt = await bcrypt.genSalt()
+          const hashed =  await bcrypt.hash(body.password, salt)
+          const user = await User.create({ email:body.email, password: hashed, salt })
+          const signed = signToken(user._id)
+          res.status(201).send({ token: signed });
+           console.log(`Usuario creado con éxito. ${body.email}`)
+           }catch (err) 
+      {
+        console.log(err)
+        res.status(500).send(err.message)
+      }
+})
+app.post('/login', async (req, res) => {
+    const { body } = req
+    try {
+        const user = await User.findOne({ email: body.email })
+        if (!user) {
+            return res.status(403).send('Usuario y/o contraseña inválida')
+        }
+
+        const isMatch = await bcrypt.compare(body.password, user.password)
+        if (!isMatch) {
+            return res.status(403).send('Usuario y/o contraseña inválida')
+        }
+
+        const signed = signToken(user._id)
+        res.status(200).json({ token: signed })
+        console.log(`${body.email} a iniciado sesión`)
+     } catch (err) {
+        console.error(err)  // Log para depuración
+        res.status(500).send('Error en el servidor, inténtalo más tarde')
+     }
+})
+
+app.listen(3000, () => {
+console.log('listening in port 3000')
+})
